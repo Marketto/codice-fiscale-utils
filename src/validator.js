435a48c72d59f9ab4c4dec0a1ad5c0646e0ea7d2
Marco <marco.ricupero@gmail.com>
2019-07-15 01:29:06 +0200

    Validator: codiceFiscale

--- src/validator.js
+++ src/validator.js
@@ -20,7 +20,7 @@ class Validator {
         if (surname) {
             matcher = Parser.surnameToCf(surname) || matcher;
         }
-        return new RegExp(`^${matcher}$`, 'i');
+        return new RegExp(`^${matcher}$`, 'iu');
     }
 
     /**
@@ -34,7 +34,7 @@ class Validator {
         if (name) {
             matcher = Parser.nameToCf(name) || matcher;
         }
-        return new RegExp(`^${matcher}$`, 'i');
+        return new RegExp(`^${matcher}$`, 'iu');
     }
 
     /**
@@ -51,7 +51,7 @@ class Validator {
                 matcher = parsedYear.replace(/\d/gu, n => `[${n}${Omocode[n]}]`);
             }
         }
-        return new RegExp(`^${matcher}$`, 'i');
+        return new RegExp(`^${matcher}$`, 'iu');
     }
 
     /**
@@ -65,7 +65,7 @@ class Validator {
         if (month) {
             matcher = Parser.monthToCf(month) || matcher;
         }
-        return new RegExp(`^${matcher}$`, 'i');
+        return new RegExp(`^${matcher}$`, 'iu');
     }
 
     /**
@@ -84,7 +84,7 @@ class Validator {
                 matcher = `(?:${matcherM})|(?:${matcherF})`;
             }
         }
-        return new RegExp(`^${matcher}$`, 'i');
+        return new RegExp(`^${matcher}$`, 'iu');
     }
 
     /**
@@ -118,7 +118,7 @@ class Validator {
                 throw new Error('[Validator.cfDayGender] Provided gender is not valid');
             }
         }
-        return new RegExp(`^${matcher}$`, 'i');
+        return new RegExp(`^${matcher}$`, 'iu');
     }
 
     /**
@@ -149,7 +149,7 @@ class Validator {
                 throw new Error('[Validator.cfDateGender] Provided gender is not valid');
             }
         }
-        return new RegExp(`^${matcher}$`, 'i');
+        return new RegExp(`^${matcher}$`, 'iu');
     }
 
     /**
@@ -172,7 +172,59 @@ class Validator {
                 matcher = '';
             }
         }
-        return new RegExp(`^${matcher}$`, 'i');
+        return new RegExp(`^${matcher}$`, 'iu');
+    }
+
+    /**
+     * Generates full CF validator based on given optional input or generic
+     * 
+     * @param {Object} input Input Object
+     * @param {string} input.surname Surname
+     * @param {string} input.name Name
+     * @param {number} [input.year] Birth Year
+     * @param {number} [input.month] Birth Month
+     * @param {number} [input.day] Birth Day
+     * @param {Date|Moment} [input.date] Birth Date
+     * @param {Gender|string} input.gender Gender M|F
+     * @param {string} input.place Place name
+     * @returns {RegExp} CodiceFiscale matcher
+     */
+    static codiceFiscale(input) {
+        let matcher = VALIDATOR.CODICE_FISCALE;
+        if (input) {
+            const parsedCf = Parser.encodeCf(input);
+
+            if (parsedCf) {
+                matcher = parsedCf.replace(/\d/gu, n => `[${n}${Omocode[n]}]`);
+            } else {
+                const { surname, name, year, month, day, date, gender, place } = input;
+                if (surname || name || year || month || day || date || gender || place) {
+                    const dtParams = Parser.parseDate(date) || Parser.yearMonthDayToDate(year, month, day);
+                    const generator = [
+                        () => this.cfSurname(surname),
+                        () => this.cfName(name),
+                        () => this.cfDateGender(dtParams, gender),
+                        () => this.cfPlace(dtParams, place)
+                    ];
+
+                    matcher = '';
+                    for (let i=0; i<generator.length; i++) {
+                        const cfPartValidator = generator[i]();
+                        if (!cfPartValidator) {
+                            return null;
+                        }
+                        const cfValue = (cfPartValidator.toString().match(/\^(.+)\$/) || [])[1];
+                        if (!cfValue) {
+                            return null;
+                        }
+                        matcher += `(?:${cfValue})`;
+                    }
+                    // Final addition of CheckDigit
+                    matcher += VALIDATOR.CHECK_DIGIT;
+                }
+            }
+        }
+        return new RegExp(`^${matcher}$`, 'iu');
     }
 }
 
--- test/validator.test.js
+++ test/validator.test.js
@@ -353,5 +353,74 @@ describe('CodiceFiscaleUtils:Validator', () => {
                 });
             });
         });
+
+        describe('codiceFiscale', () => {
+            describe('Generic Validator', () => {
+                const cfDateValidator = Validator.codiceFiscale();
+                it ('Should validate VRNGNY07D68C351V', () => {
+                    cfDateValidator.test('VRNGNY07D68C351V').should.be.ok;
+                });
+                it ('Should validate MRNMIA02E45L219X', () => {
+                    cfDateValidator.test('MRNMIA02E45L219X').should.be.ok;
+                });
+            });
+
+            describe('Partial validator', () => {
+                it('Should match VRNGNY97A65C351V', () => {
+                    Validator.codiceFiscale({
+                        surname: 'Veronesi',
+                        name: 'Genny',
+                        gender: 'F',
+                        place: 'Catania'
+                    }).test('VRNGNY97A65C351V').should.be.true;
+                });
+                it('Should not match VRNGNY97A35C351V', () => {
+                    Validator.codiceFiscale({
+                        surname: 'Veronesi',
+                        name: 'Genny',
+                        gender: 'F',
+                        place: 'Catania'
+                    }).test('VRNGNY97A35C351V').should.be.false;
+                });
+                it('Should match KRNZEA02E45L219X', () => {
+                    Validator.codiceFiscale({
+                        year: 1902,
+                        month: 4,
+                        day: 5,
+                        gender: 'F',
+                        place: 'Torino'
+                    }).test('KRNZEA02E45L219X').should.be.true;
+                });
+                it('Should not match MRNMIA03E45L219X', () => {
+                    Validator.codiceFiscale({ year: 1902, month: 4, day: 5, gender: 'F', place: 'Torino' })
+                        .test('MRNMIA03E45L219X').should.be.false;
+                });
+            });
+
+            describe('Specific validator', () => {
+                it('Should match VRNGNY07D68C351V', () => {
+                    Validator.codiceFiscale({
+                        surname: 'Veronesi',
+                        name: 'Genny',
+                        year: 1907,
+                        month: 3,
+                        day: 28,
+                        gender: 'F',
+                        place: 'Catania'
+                    }).test('VRNGNY07D68C351V').should.be.true;
+                });
+                it('Should match MRNMIA02E45L219X', () => {
+                    Validator.codiceFiscale({
+                        surname: 'Marin',
+                        name: 'Mia',
+                        year: 1902,
+                        month: 4,
+                        day: 5,
+                        gender: 'F',
+                        place: 'Torino'
+                    }).test('MRNMIA02E45L219X').should.be.true;
+                });
+            });
+        });
     });
 });
