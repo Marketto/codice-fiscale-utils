8c948eb75f8b08832789aedce8b62f8c0cbfa401
Marco <marco.ricupero@gmail.com>
2019-07-29 20:44:06 +0200

    codice-fiscale-utils-vue-demo

---
+++ demo/vue/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Codice Fiscale Utils</h1>
+    <h2>Vue Demo</h2>
+    <CodiceFiscale/>
+  </div>
+</template>
+
+<script>
+import CodiceFiscale from './components/CodiceFiscale.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CodiceFiscale
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo/vue/src/main.js
@@ -0,0 +1,13 @@
+import 'moment'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import App from './App.vue'
+
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
