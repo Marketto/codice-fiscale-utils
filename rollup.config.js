37eec19a629dc12f24c63b4273970f894a8da26b
Marco Ricupero <marco.ricupero@gmail.com>
2019-12-31 17:50:44 +0100

    ES -> TS - Config migration

--- rollup.config.js
+++ rollup.config.js
@@ -1,65 +1,129 @@
+import rollupPluginTs from '@wessberg/rollup-plugin-ts';
 import pkg from './package.json';
+import tsconfig from './tsconfig.json';
 import { terser } from 'rollup-plugin-terser';
-import pluginJson from 'rollup-plugin-json';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import builtins from 'rollup-plugin-node-builtins';
+import jsonPlugin from '@rollup/plugin-json';
 import license from 'rollup-plugin-license';
 import path from 'path';
 
-
 const baseConf = {
-    input: 'src/',
     external: [
         ...Object.keys(pkg.dependencies || {})
     ],
+    input: 'src/classes/codice-fiscale-utils.class.ts',
     output: {
+        exports: 'default',
         name: 'CodiceFiscaleUtils',
-        sourcemap: true,
-        globals: {
-            moment: 'moment',
-            '@marketto/diacritic-remover': 'DiacriticRemover'
-        }
+        sourceMap: true,
     },
     plugins: [
-        nodeResolve(),
-        pluginJson(),
-        babel({
-            exclude: 'node_modules/**' // only transpile our source code
-        }),
-        terser(),
         license({
-            cwd: __dirname,
-            banner: {        
+            banner: {
                 content: {
                     file: path.join('.', 'src/banner'),
                 },
-            },        
+            },
+            cwd: __dirname,
+            sourcemap: true,
         }),
-    ]
+    ],
 };
+
+const rollupCjsConf = rollupPluginTs({
+    tsconfig: {
+        ...tsconfig.compilerOptions,
+        declaration: false,
+    },
+});
+const rollupModuleConf = rollupPluginTs({
+    tsconfig: {
+        ...tsconfig.compilerOptions,
+        declaration: true,
+        module: 'ESNext',
+        target: 'ESNext',
+    },
+});
+const rollupBrowserConf = rollupPluginTs({
+    tsconfig: {
+        ...tsconfig.compilerOptions,
+        declaration: false,
+        module: 'iife',
+        target: 'ES2015',
+        namedExports: {
+            
+        }
+    },
+});
 
 export default [
     {
         ...baseConf,
-        output: [
-            {
-                file: pkg.main,
-                format: 'cjs',
-                ...baseConf.output
-            },
-            {
-                file: pkg.module,
-                format: 'esm',
-                ...baseConf.output
-            },
-            {
-                file: 'dist/codice-fiscale-utils.bundle.min.js',
-                format: 'iife',
-                ...baseConf.output
-            }
-        ],
+        output: {
+            ...baseConf.output,
+            file: pkg.main,
+            format: 'cjs',
+        },
         plugins: [
-            ...baseConf.plugins
-        ]
-    }
+            builtins(),
+            rollupCjsConf,
+            jsonPlugin({
+                namedExports: false,
+                preferConst: true,
+            }),
+            ...baseConf.plugins,
+        ],
+    },
+    {
+        ...baseConf,
+        output: {
+            ...baseConf.output,
+            file: pkg.module,
+            format: 'esm',
+        },
+        plugins: [
+            builtins(),
+            rollupModuleConf,
+            jsonPlugin({
+                namedExports: false,
+                preferConst: true,
+            }),
+            ...baseConf.plugins,
+        ],
+    },
+    {
+        ...baseConf,
+        output: {
+            ...baseConf.output,
+            file: 'dist/codice-fiscale-utils.bundle.js',
+            format: 'iife',
+        },
+        plugins: [
+            builtins(),
+            rollupBrowserConf,
+            jsonPlugin({
+                namedExports: false,
+                preferConst: true,
+            }),
+            ...baseConf.plugins,
+        ],
+    },
+    {
+        ...baseConf,
+        output: {
+            ...baseConf.output,
+            file: 'dist/codice-fiscale-utils.bundle.min.js',
+            format: 'iife',
+        },
+        plugins: [
+            builtins(),
+            rollupBrowserConf,
+            jsonPlugin({
+                namedExports: false,
+                preferConst: true,
+            }),
+            terser(),
+            ...baseConf.plugins,
+        ],
+    },
 ];
