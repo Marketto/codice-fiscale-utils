5e3665ed9ea15bfc9991d9198fcb12d9b5490aa9
Marco Ricupero <marco.ricupero@gmail.com>
2019-12-31 17:50:57 +0100

    ES -> TS - Test migration

--- test/utils.ts
+++ test/utils.ts
@@ -1,10 +1,10 @@
-import chai from 'chai';
-import chaiThings from 'chai-things';
+import chai from "chai";
+import chaiThings from "chai-things";
 chai.use(chaiThings);
 chai.should();
 const { expect } = chai;
 
 export {
+    expect,
     chai,
-    expect
 };
