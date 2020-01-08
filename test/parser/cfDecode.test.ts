83d9e9f9402d952dad94aef556ebd72fb062aa66
Marco Ricupero <marco.ricupero@gmail.com>
2020-01-08 20:31:03 +0100

    Implemented DeOmocode in parsing methods

--- test/parser/cfDecode.test.ts
+++ test/parser/cfDecode.test.ts
@@ -3,8 +3,8 @@ import { expect } from "../utils";
 
 export default async () => {
     describe("cfDecode", () => {
-        describe("Lowercase", () => {
-            it("Should return matching infos from VRNGNY07D68C351V", () => {
+        describe("Uppercase", () => {
+            it("Should return matching infos for VRNGNY07D68C351V", () => {
                 const decoded = Parser.cfDecode("VRNGNY07D68C351V");
                 expect(decoded).to.be.a("object");
                 expect(decoded.lastName).to.be.equal("V*R*N*");
@@ -14,8 +14,9 @@ export default async () => {
                 expect(decoded.day).to.be.equal(28);
                 expect(decoded.gender).to.be.equal("F");
                 expect(decoded.place).to.be.equalIgnoreCase("CATANIA");
+                //expect(decoded.omocode).to.be.equal(0);
             });
-            it("Should return matching infos from MRNMIA02E45L219X", () => {
+            it("Should return matching infos for MRNMIA02E45L219X", () => {
                 const decoded = Parser.cfDecode("MRNMIA02E45L219X");
                 expect(decoded).to.be.a("object");
                 expect(decoded.lastName).to.be.equal("M*R*N*");
@@ -25,10 +26,11 @@ export default async () => {
                 expect(decoded.day).to.be.equal(5);
                 expect(decoded.gender).to.be.equal("F");
                 expect(decoded.place).to.be.equalIgnoreCase("TORINO");
+                //expect(decoded.omocode).to.be.equal(0);
             });
         });
         describe("Lowercase", () => {
-            it("Should return matching infos from vrngny07d68c351v", () => {
+            it("Should return matching infos for vrngny07d68c351v", () => {
                 const decoded = Parser.cfDecode("vrngny07d68c351v");
                 expect(decoded).to.be.a("object");
                 expect(decoded.lastName).to.be.equal("v*r*n*");
@@ -38,8 +40,9 @@ export default async () => {
                 expect(decoded.day).to.be.equal(28);
                 expect(decoded.gender).to.be.equal("F");
                 expect(decoded.place).to.be.equalIgnoreCase("CATANIA");
+                //expect(decoded.omocode).to.be.equal(0);
             });
-            it("Should return matching infos from mrnmia02e45l219x", () => {
+            it("Should return matching infos for mrnmia02e45l219x", () => {
                 const decoded = Parser.cfDecode("mrnmia02e45l219x");
                 expect(decoded).to.be.a("object");
                 expect(decoded.lastName).to.be.equal("m*r*n*");
@@ -49,6 +52,21 @@ export default async () => {
                 expect(decoded.day).to.be.equal(5);
                 expect(decoded.gender).to.be.equal("F");
                 expect(decoded.place).to.be.equalIgnoreCase("TORINO");
+                //expect(decoded.omocode).to.be.equal(0);
+            });
+        });
+        describe("Omocode", () => {
+            it("Should return matching infos for VRNGNYLTDSUCPRMT", () => {
+                const decoded = Parser.cfDecode("VRNGNYLTDSUCPRMT");
+                expect(decoded).to.be.a("object");
+                expect(decoded.lastName).to.be.equal("V*R*N*");
+                expect(decoded.firstName).to.be.equal("G*N*Y*");
+                expect(decoded.year).to.be.equal(2007);
+                expect(decoded.month).to.be.equal(3);
+                expect(decoded.day).to.be.equal(28);
+                expect(decoded.gender).to.be.equal("F");
+                expect(decoded.place).to.be.equalIgnoreCase("CATANIA");
+                //expect(decoded.omocode).to.be.equal(2);
             });
         });
     });
