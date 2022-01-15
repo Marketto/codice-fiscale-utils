4f63df51a96563a830b00bbcb148a17db610f25d
Marco Ricupero <marco.ricupero@gmail.com>
2022-01-15 12:08:58 +0100

    Dependencies update & version bump

--- tools/assets-generator.ts
+++ tools/assets-generator.ts
@@ -1,3 +1,4 @@
+import dotenv from "dotenv";
 import moment from "moment";
 const initialTimer = moment();
 import fs from "fs";
@@ -5,6 +6,8 @@ import path from "path";
 import { promisify } from "util";
 import { exportDataset } from "./generators/dataset-generator";
 import { exportLicenses } from "./generators/licenses-generator";
+
+dotenv.config({ path: path.join(__dirname, "./.env") });
 
 const DEST_PATH = path.join(__dirname, "../asset");
 const DEFAULT_DATASET_FILE_PATH = path.join(DEST_PATH, "cities-countries.ts");
